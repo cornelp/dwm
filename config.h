bb04fc051ba70cd6ba0b68aeaef7d010be620897
Cornel <cornel.paduraru87@gmail.com>
2020-11-24 23:13:43 +0200

    Changed color for selected item.

--- config.h
+++ config.h
@@ -17,10 +17,11 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_yellow[]      = "#ffd300";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_yellow  },
 };
 
 /* tagging */
